09d7fdf908fdb1e7cf03a7b2a6e7ce30f760579d
CarlosWazlawick <125685293+CarlosWazlawick@users.noreply.github.com>
2023-10-03 22:04:51 -0300

    Add files via upload

---
+++ Aula 30.09.23/index.js
@@ -0,0 +1,45 @@
+let nome = prompt("Digite seu nome:");
+let anoNascimento = prompt("Insira seu ano de nascimento: ");
+let anoAtual = prompt("Insira o ano atual");
+let idade = anoAtual - anoNascimento;
+let maiorDeIdade = idade >= 18;
+let idade2050 = 2050 - anoNascimento;
+
+alert(`Nome: ${nome}`);
+alert(`Idade: ${idade} anos`);
+alert(`Maior de idade: ${maiorDeIdade}`);
+alert(`Idade em 2050: ${idade2050} anos`);
+
+let a = Number(prompt("Insira um valor: "));
+let b = Number(prompt("Insira outro valor: "));
+alert(a + b);
+
+let result1 = 3 + 4;
+let result2 = (3 * 5) / 2;
+let result3 = (5 - 4) * -1;
+let result4 = 234 % 5;
+alert(result1);
+alert(result2);
+alert(result3);
+alert(result4);
+
+let primeiroNumero = 15;
+let segundoNumero = 20;
+alert(primeiroNumero === segundoNumero);
+alert(primeiroNumero !== segundoNumero);
+alert(primeiroNumero >= segundoNumero);
+alert(primeiroNumero <= segundoNumero);
+
+let x = true;
+let y = false;
+let z = true;
+alert(x && y);
+alert(y && z);
+alert(x && z);
+
+let p = true;
+let q = false;
+let r = true;
+alert(p || q);
+alert(q || r);
+alert(p || r);
